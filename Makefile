ebcb2d1de031b5bcaa93e799bd4b74ce8fb841d0
odo <florian.odronitz@wooga.net>
2013-12-11 10:28:42 +0100

    reporting time, adding Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+console:
+	erl -smp enable -pz ebin deps/*/ebin -s erlkafka
--- src/erlkafka_profile.erl
+++ src/erlkafka_profile.erl
@@ -3,13 +3,16 @@
 -compile([export_all]).
 
 from_file(Topic, FN, Count) ->
+    StartTime = now(),
     {ok, File} = file:open(FN, [read, binary]),
     LineCount = round(Count / 100.0),
     {_, _, Lines} = lists:foldl(fun next_line/2, {FN, File, []}, lists:seq(1, LineCount)),
     file:close(File),
     [[gen_server:call(ballermann:pid(producer_pool), {add, Topic, L})||L<-Lines]||_<-lists:seq(1, 100)],
     Sync = fun(Pid) -> gen_server:call(Pid, sync) end,
-    ballermann:apply_within(producer_pool, {erlang, list_to_atom, ["sync"]}, Sync).
+    ballermann:apply_within(producer_pool, {erlang, list_to_atom, ["sync"]}, Sync),
+    Elapsed = timer:now_diff(now(), StartTime) / 1000000,
+    io:format("\n~p seconds for ~p messages: ~p ops\n", [Elapsed, Count, Count / Elapsed]).
 
 next_line(_, {FN, F, Acc}) ->
   case io:get_line(F, "") of
