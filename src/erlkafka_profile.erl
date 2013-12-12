bae85f1614b65d2f43b6f2e6f192bd367f44c8f3
odo <florian.odronitz@wooga.net>
2013-12-12 18:28:43 +0100

    adding predefined rates

--- src/erlkafka_profile.erl
+++ src/erlkafka_profile.erl
@@ -1,21 +1,43 @@
 -module(erlkafka_profile).
 
--compile([export_all]).
-
-from_file(Topic, FN, Count) when is_binary(Topic) ->
-    from_file([Topic], FN, Count);
+-compile([export_all, {inline, [{add_and_wait, 2}]}]).
 
 from_file(Topics, FN, Count) ->
-    StartTime = now(),
+    from_file(Topics, FN, Count, undefined).
+
+from_file(Topics, FN, Count, Rate) ->
     {ok, File} = file:open(FN, [read, binary]),
-    LineCount = round(Count / 100.0),
+    Series = 10,
+    LineCount = round(Count / Series * 1.0),
     {_, _, Lines} = lists:foldl(fun next_line/2, {FN, File, []}, lists:seq(1, LineCount)),
     file:close(File),
-    [[gen_server:call(ballermann:pid(producer_pool), {add, one_of(Topics), L})||L<-Lines]||_<-lists:seq(1, 100)],
+    StartTime = now(),
+    case Rate of
+        undefined ->
+            [[gen_server:call(ballermann:pid(producer_pool), {add, one_of(Topics), L})||L<-Lines]||_<-lists:seq(1, Series)];
+        _ ->
+            TimePerMsg = 1.0 / Rate * 1000000.0, %micros
+            [lists:foldl(fun add_and_wait/2, {Topics, TimePerMsg, 0, os:timestamp()}, Lines)||_<-lists:seq(0, (Series - 1))]
+    end,
     Sync = fun(Pid) -> gen_server:call(Pid, sync) end,
     ballermann:apply_within(producer_pool, {erlang, list_to_atom, ["sync"]}, Sync),
     Elapsed = timer:now_diff(now(), StartTime) / 1000000,
     io:format("\n~p seconds for ~p messages: ~p ops\n", [Elapsed, Count, Count / Elapsed]).
+
+add_and_wait(L, {Topics, TimePerMsg, N, SeriesStartTime}) ->
+    case N rem 1000 =:= 0 of
+        true ->
+            SeriesTimeElapse = timer:now_diff(os:timestamp(), SeriesStartTime),
+            case (TimePerMsg * N * 1.0) - SeriesTimeElapse * 1.0 of
+                TTS when TTS > 0.0 ->
+                    timer:sleep(round(TTS/1000.0));
+                _ -> noop
+            end;
+        false -> noop
+    end,
+    gen_server:call(ballermann:pid(producer_pool), {add, one_of(Topics), L}),
+    {Topics, TimePerMsg, N + 1, SeriesStartTime}.
+
 
 next_line(_, {FN, F, Acc}) ->
   case io:get_line(F, "") of
